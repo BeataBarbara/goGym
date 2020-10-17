58b532c175c6fddda12f17e62c958a20b2759c7f
BeataBarbara <skorusbeata@gmail.com>
2020-10-17 23:50:33 +0200

    squash added

--- src/app/squash/squash.component.ts
+++ src/app/squash/squash.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-squash',
-  templateUrl: './squash.component.html',
+  templateUrl: './squash.component.svg',
   styleUrls: ['./squash.component.css']
 })
 export class SquashComponent implements OnInit {
+  velocity =1500;
 
-  constructor() { }
+  @ViewChild('pos1') pos1: ElementRef;
+  @ViewChild('pos2') pos2: ElementRef;
+  @ViewChild('pos3') pos3: ElementRef;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.setInt(this.velocity)
+  }
+
+  setInt (vel) {
+    setInterval (() => this.playSquash(), vel)
+  }
+  playSquash() {
+    if (!this.pos1.nativeElement.classList.contains('hide')) {
+      this.pos1.nativeElement.classList.add('hide')
+      this.pos2.nativeElement.classList.remove('hide')       
+    } else if (!this.pos2.nativeElement.classList.contains('hide')) {
+      this.pos2.nativeElement.classList.add('hide')
+      this.pos3.nativeElement.classList.remove('hide')       
+    }  else  {
+      this.pos3.nativeElement.classList.add('hide')
+      this.pos1.nativeElement.classList.remove('hide')       
+    } 
   }
 
 }
